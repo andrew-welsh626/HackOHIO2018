f920d421ac3110d0287f5ab2f27bfb767356f0d2
Andrew Davis <pulpdrew@gmail.com>
2018-10-27 09:29:03 -0400

    Initial Commit

---
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/MainActivity.java
@@ -0,0 +1,13 @@
+package vote.hackohio.com.ezfunvoting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
