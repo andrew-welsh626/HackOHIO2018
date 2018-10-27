560c47aa0f79486d3b8c0ef1a4670de105f8ad0b
Andrew Davis <pulpdrew@gmail.com>
2018-10-27 16:09:36 -0400

    Display Group ID

--- app/src/main/java/vote/hackohio/com/ezfunvoting/VoteActivity.java
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/VoteActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -22,7 +23,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,9 +34,10 @@ public class VoteActivity extends AppCompatActivity {
      * Instance variables
      */
     private RecyclerView votingRecyclerView;
+    private TextView groupIdTextView;
     private OptionsAdapter adapter;
     private List<OptionModel> options = new ArrayList<>();
-    private String groupName;
+    private String groupID;
     private String userID;
 
     /**
@@ -52,9 +53,12 @@ public class VoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vote);
 
         votingRecyclerView = findViewById(R.id.rv_vote);
+        groupIdTextView = findViewById(R.id.group_id_tv);
+
         createOrReadUID();
 
-        this.groupName = getIntent().getStringExtra(GROUP_NAME_EXTRA_KEY);
+        this.groupID = getIntent().getStringExtra(GROUP_NAME_EXTRA_KEY);
+        groupIdTextView.setText(groupID);
 
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(getItemTouchHelper());
 
@@ -103,7 +107,7 @@ public class VoteActivity extends AppCompatActivity {
      */
     private void sendVotesToDatabase() {
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/groups/" + groupName);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/groups/" + groupID);
         for (int i = 0; i < options.size(); i++) {
             OptionModel option = options.get(i);
             option.rankings.put(this.userID, i + 1);
@@ -117,14 +121,14 @@ public class VoteActivity extends AppCompatActivity {
      */
     private void setupDataListener() {
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("groups/" + this.groupName);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("groups/" + this.groupID);
         ref.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
                 if (dataSnapshot.getKey().equals("name")) {
                     getSupportActionBar().setTitle((String) dataSnapshot.getValue());
-                } else {
+                } else if (!dataSnapshot.getKey().equals("id")) {
                     OptionModel option = dataSnapshot.getValue(OptionModel.class);
                     option.setId(dataSnapshot.getKey());
                     options.add(option);
@@ -162,7 +166,7 @@ public class VoteActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.add_option:
                 Intent intent = new Intent(this, AddActivity.class);
-                intent.putExtra(GROUP_NAME_EXTRA_KEY, groupName);
+                intent.putExtra(GROUP_NAME_EXTRA_KEY, groupID);
                 startActivity(intent);
                 return true;
             case R.id.send_votes:
