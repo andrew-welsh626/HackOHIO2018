bdbce37c89af2a704789fd51e0e2280d347f3955
DanaShkokani <shkokani.1@osu.edu>
2018-10-27 14:12:50 -0400

    n

--- app/src/main/java/vote/hackohio/com/ezfunvoting/MainActivity.java
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/MainActivity.java
@@ -2,9 +2,9 @@ package vote.hackohio.com.ezfunvoting;
 import android.content.Intent;
 import android.view.View;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, VoteActivity.class);
-        intent.putExtra(VoteActivity.GROUP_NAME_EXTRA_KEY, "name 1");
-        startActivity(intent);
-
+Button create = findViewById(R.id.button2);
+        create.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                createPage(v);
+            }
+        });
+        Button join = findViewById(R.id.button3);
+        join.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                JoinActivity(v);
+            }
+        });
     }
 
-    public void joinPage(View v) {
-        Intent joinActivity = new Intent(MainActivity.this, JoinActivity.class);
-        startActivity(joinActivity);
+    public void JoinActivity(View v) {
+        Intent JoinActivity = new Intent(MainActivity.this, JoinActivity.class);
+        startActivity(JoinActivity);
     }
     public void createPage(View v) {
         Intent createPage = new Intent(MainActivity.this, createPage.class);
