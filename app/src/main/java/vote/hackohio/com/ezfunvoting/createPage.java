41109f4350f4fc64bf6581959902e07ff6a00d34
Alex <7Alex7Li@gmail.com>
2018-10-27 12:16:59 -0400

    Connected Firebase

---
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/createPage.java
@@ -0,0 +1,17 @@
+package vote.hackohio.com.ezfunvoting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class createPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+    /** Called when the user touches the join button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
+    }
+}
