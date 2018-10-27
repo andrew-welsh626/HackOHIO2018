541addf410a2687bb24ea13331ae3d639848071e
Alex <7Alex7Li@gmail.com>
2018-10-27 11:42:24 -0400

    Connected Firebase, Fiddled with dependencies

--- app/src/main/java/vote/hackohio/com/ezfunvoting/AddActivity.java
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/AddActivity.java
@@ -2,8 +2,15 @@ package vote.hackohio.com.ezfunvoting;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -22,6 +29,10 @@ public class AddActivity extends AppCompatActivity {
 
     protected void addOption(View v){
         String optionName = findViewById(R.id.editText).toString();
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("groups/swanky-unarmed-heptagons/Sent From Andrio Studio");
 
+        myRef.setValue("Food Eat Yum");
     }
 }
