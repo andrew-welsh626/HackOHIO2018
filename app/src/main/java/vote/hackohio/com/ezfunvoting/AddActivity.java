cf1c5a0bdbde2de63120f06f401e7957940236e0
Alex <7Alex7Li@gmail.com>
2018-10-27 11:02:32 -0400

    Added the pages add and vote

--- app/src/main/java/vote/hackohio/com/ezfunvoting/AddActivity.java
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/AddActivity.java
@@ -1,7 +1,9 @@
 package vote.hackohio.com.ezfunvoting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        final Button button = findViewById(R.id.addbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addOption(v);
+            }
+        });
+    }
+
+    protected void addOption(View v){
+        String optionName = findViewById(R.id.editText).toString();
+
     }
 }
