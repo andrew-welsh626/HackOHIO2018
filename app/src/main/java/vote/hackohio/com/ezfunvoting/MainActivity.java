d573184b2da13a42ab4bba21e063d654b5237588
DanaShkokani <shkokani.1@osu.edu>
2018-10-27 12:02:38 -0400

    n

--- app/src/main/java/vote/hackohio/com/ezfunvoting/MainActivity.java
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/MainActivity.java
@@ -1,4 +1,6 @@
 package vote.hackohio.com.ezfunvoting;
+import android.content.Intent;
+import android.view.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void joinPage(View v) {
+        Intent joinPage = new Intent(MainActivity.this, joinPage.class);
+        startActivity(joinPage);
+    }
+    public void createPage(View v) {
+        Intent createPage = new Intent(MainActivity.this, createPage.class);
+        startActivity(createPage);
     }
 }
---
+++ app/src/main/java/vote/hackohio/com/ezfunvoting/createPage.java
@@ -0,0 +1,21 @@
+package vote.hackohio.com.ezfunvoting;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class createPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+    /** Called when the user touches the join button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
+    }
+}
